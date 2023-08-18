74181e091f552167e3d9ceac1908c2a5faf118cc
jrshields86 <jrshields86@gmail.com>
2023-08-18 13:59:11 -0600

    second commit

--- froyo.js
+++ froyo.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+
+const inputFroyo = window.prompt('Please enter a list of froyo flavors separated by commas', 'vanilla, vanilla, vanilla, strawberry, coffee, coffee');
+const froyoArray = inputFroyo.split(',');
+const stats = {
+
+};
+
+for(let i = 0; i < froyoArray.length; i++){
+    const flavor = froyoArray[i];
+    if(stats[flavor] === undefined){
+        stats[flavor] = 0;
+    }
+    stats[flavor] = stats[flavor] + 1;
+}
+console.log(stats);
